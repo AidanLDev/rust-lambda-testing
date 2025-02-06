fbf59775f01f5fe1b49a3a917db8ba80866c899b
Aidan Lowson <dev@aidanlowson.com>
2025-02-06 22:16:57 +0000

    Getting data

--- src/http_handler.rs
+++ src/http_handler.rs
@@ -1,7 +1,8 @@
 use aws_config::meta::region::RegionProviderChain;
 use aws_config::BehaviorVersion;
-use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::{types::AttributeValue, Client};
 use lambda_http::{Body, Error, Request, Response};
+use std::collections::HashMap;
 
 /// This is the main body for the function.
 /// Write your code inside it.
@@ -21,8 +22,40 @@ pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, E
     let client = Client::new(&config);
     let email_list_table = String::from("NewsLetterSubscribers");
 
+    let raw_items = get_all_items(&client, &email_list_table).await?;
+
+    println!("raw_items {:?} ", raw_items);
+
     Ok(Response::builder()
         .status(200)
-        .body(Body::from(format!("working...: ")))
+        .body(Body::from("working...: "))
         .expect("Failed to construct response"))
 }
+
+async fn get_all_items(client: &Client, table_name: &String) -> Result<Vec<HashMap<String, AttributeValue>>, aws_sdk_dynamodb::Error> {
+    let mut items = Vec::new();
+    let mut last_evaluated_key = None;
+
+    loop {
+        let resp = client
+            .scan()
+            .table_name(table_name)
+            .set_exclusive_start_key(last_evaluated_key)
+            .send()
+            .await?;
+
+        if let Some(new_items) = resp.items {
+            items.extend(new_items);
+        }
+
+        last_evaluated_key = resp.last_evaluated_key;
+
+        if last_evaluated_key.is_none() {
+            break;
+        }
+    }
+
+    Ok(items)
+
+} 
+
