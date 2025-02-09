cc4e8aa97ac1fddabab9d5eb46f96b0fdf72c2a2
Aidan Lowson <dev@aidanlowson.com>
2025-02-09 00:25:26 +0000

    Function works

--- src/http_handler.rs
+++ src/http_handler.rs
@@ -1,18 +1,19 @@
 use aws_config::meta::region::RegionProviderChain;
 use aws_config::BehaviorVersion;
-use aws_sdk_dynamodb::{types::AttributeValue, Client};
+use aws_sdk_dynamodb::{types::AttributeValue, Client, Error as Dynamo_Error};
 use aws_sdk_ses::{
-    operation::send_email::{SendEmailError, SendEmailOutput},
-    Client as Ses_Client,
+    operation::send_email::SendEmailOutput,
+    types::{Body as Email_Body, Content, Destination, Message},
+    Client as Ses_Client, Error as Ses_Error,
 };
-use lambda_http::{Body, Error, Request, Response};
+use lambda_http::{Body, Error as Lambda_Error, Request, Response};
 use std::collections::HashMap;
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, Lambda_Error> {
     // Extract some useful information from the request
     let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
 
@@ -43,6 +44,10 @@ pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, E
 
     println!("Emails: {:?}", emails);
 
+    println!("About to send mail!");
+
+    send_email(&ses_client, vec![String::from("dev@aidanlowson.com")]).await?;
+
     Ok(Response::builder()
         .status(200)
         .body(Body::from("working...: "))
@@ -52,7 +57,7 @@ pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, E
 async fn get_all_items(
     client: &Client,
     table_name: &String,
-) -> Result<Vec<HashMap<String, AttributeValue>>, aws_sdk_dynamodb::Error> {
+) -> Result<Vec<HashMap<String, AttributeValue>>, Dynamo_Error> {
     let mut items = Vec::new();
     let mut last_evaluated_key = None;
 
@@ -80,45 +85,34 @@ async fn get_all_items(
 
 async fn send_email(
     ses_client: &Ses_Client,
-    recipients: &[&str],
-) -> Result<SendEmailOutput, Error<SendEmailError>> {
+    recipients: Vec<String>,
+) -> Result<SendEmailOutput, Ses_Error> {
     let sender = "dev@aidanlowson.com";
-    let subject = "Hello World";
-    let body_text = "Hello World!";
-    let body_html = "<html><body><h1>Hello World!</h1></body></html>";
+    let subject = String::from("Hello World");
+    let body_text = String::from("Hello World!");
+    let body_html = String::from("<html><body><h1>Hello World!</h1></body></html>");
 
-    let destination = aws_sdk_ses::types::Destination::builder()
-        .to_addresses(
-            recipients
-                .iter()
-                .map(|&r| r.to_string())
-                .collect::<Vec<_>>(),
-        )
+    let destination = Destination::builder()
+        .set_bcc_addresses(Some(recipients))
         .build();
 
-    let message = aws_sdk_ses::types::Message::builder()
-        .subject(subject)
-        .body(
-            aws_sdk_ses::types::Body::builder()
-                .text(
-                    aws_sdk_ses::types::Content::builder()
-                        .data(body_text)
-                        .build(),
-                )
-                .html(
-                    aws_sdk_ses::types::Content::builder()
-                        .data(body_html)
+    let send_email_builder = ses_client
+        .send_email()
+        .destination(destination)
+        .message(
+            Message::builder()
+                .subject(Content::builder().data(subject).build()?)
+                .body(
+                    Email_Body::builder()
+                        .text(Content::builder().data(body_text).build()?)
+                        .html(Content::builder().data(body_html).build()?)
                         .build(),
                 )
                 .build(),
         )
-        .build();
+        .source(sender);
 
-    let email_request = ses_client
-        .send_email()
-        .source(sender)
-        .destination(destination)
-        .message(message);
+    let response = send_email_builder.send().await?;
 
-    Ok(email_request.send().await?)
+    Ok(response)
 }
